2b95fd5e274756191937a82d043810e0bbb52136
MITH Sovanda <vanda.can.read@gmail.com>
2024-04-14 17:17:19 +0700

    implement loading for admin dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -26,10 +26,30 @@ async function getUserData() {
       _sum: { pricePaidInCents: true },
     }),
   ]);
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  };
+}
+
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+    db.product.count({ where: { isAvailableForPurchase: false } }),
+  ]);
+
+  return { activeCount, inactiveCount };
 }
 
 export default async function AdminDashboard() {
-  const salesData = await getSalesData();
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ]);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -39,9 +59,16 @@ export default async function AdminDashboard() {
         body={formatCurrency(salesData.amount)}
       />
       <DashboardCard
-        title="Customer"
-        subtitle={`${formatCurrency()}} Avverage Value`}
-        body={formatNumber()}
+        title="Customers"
+        subtitle={`${formatCurrency(
+          userData.averageValuePerUser
+        )} Average Value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+        body={formatNumber(productData.activeCount)}
       />
     </div>
   );
